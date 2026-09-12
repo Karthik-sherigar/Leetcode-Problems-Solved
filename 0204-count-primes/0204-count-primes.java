1524b9c5758c33b196deb89421df6ba2881828f4
karthik sherigar <153983715+Karthik-sherigar@users.noreply.github.com>
2026-09-12 20:22:56 +0530

    Time: 715 ms (23.54%), Space: 79.9 MB (19.27%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime, true);
+        if (n > 0) prime[0] = false;
+        if (n > 1) prime[1] = false;
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i)
+                    prime[j] = false;
+            }
+        }
+        int count = 0;
+        for (boolean p : prime)
+            if (p) count++;
+        return count;
+    }
+}
