3be8a2c2cba08f05f2f80879eb40416a43f62829
karthik sherigar <153983715+Karthik-sherigar@users.noreply.github.com>
2026-09-20 18:03:59 +0530

    Time: 1 ms (100%), Space: 43.8 MB (91.21%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int res = 0;
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            int reverseValue = 'z' - ch+1 ;
+            int ans = reverseValue*(i+1);
+            res=res+ans;
+        }
+        return res;
+    }
+}
